293f6f51354ad50a3ab5fcd79cb1df1d20a97820
M Rojer Fadli <fadlirojer49@gmail.com>
2026-09-15 16:13:00 +0700

    Add files via upload

---
+++ AI.md
@@ -0,0 +1,9 @@
+# Penggunaan AI
+
+AI digunakan sebagai tutor untuk membantu memahami instruksi
+praktikum serta memberikan penjelasan mengenai HTML dan CSS.
+
+AI tidak digunakan untuk menggantikan proses pembelajaran.
+Kode dipahami dan dikerjakan kembali oleh mahasiswa.
+
+Tanggal: 15 September 2026
---
+++ data.js
@@ -0,0 +1,214 @@
+const dataMahasiswa = [
+    {
+        nama: "M Rojer Fadli",
+        nim: "20252210177",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 92
+    },
+    {
+        nama: "Andi Saputra",
+        nim: "20252210001",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 85
+    },
+    {
+        nama: "Budi Santoso",
+        nim: "20252210002",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 88
+    },
+    {
+        nama: "Citra Lestari",
+        nim: "20252210003",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 90
+    },
+    {
+        nama: "Dewi Anggraini",
+        nim: "20252210004",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 87
+    },
+    {
+        nama: "Eko Pratama",
+        nim: "20252210005",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 82
+    },
+    {
+        nama: "Fajar Ramadhan",
+        nim: "20252210006",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 89
+    },
+    {
+        nama: "Gita Permata",
+        nim: "20252210007",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 91
+    },
+    {
+        nama: "Hendra Wijaya",
+        nim: "20252210008",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 84
+    },
+    {
+        nama: "Indah Sari",
+        nim: "20252210009",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 86
+    },
+    {
+        nama: "Joko Susanto",
+        nim: "20252210010",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 80
+    },
+    {
+        nama: "Kurniawan Putra",
+        nim: "20252210011",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 83
+    },
+    {
+        nama: "Lina Marlina",
+        nim: "20252210012",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 94
+    },
+    {
+        nama: "Muhammad Rizky",
+        nim: "20252210013",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 88
+    },
+    {
+        nama: "Nur Aisyah",
+        nim: "20252210014",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 93
+    },
+    {
+        nama: "Oki Setiawan",
+        nim: "20252210015",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 81
+    },
+    {
+        nama: "Putri Amelia",
+        nim: "20252210016",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 90
+    },
+    {
+        nama: "Rian Kurnia",
+        nim: "20252210017",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 86
+    },
+    {
+        nama: "Siti Rahma",
+        nim: "20252210018",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 95
+    },
+    {
+        nama: "Taufik Hidayat",
+        nim: "20252210019",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 84
+    },
+    {
+        nama: "Agus Setiawan",
+        nim: "20252210020",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 87
+    },
+    {
+        nama: "Bella Putri",
+        nim: "20252210021",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 92
+    },
+    {
+        nama: "Dimas Prakoso",
+        nim: "20252210022",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 89
+    },
+    {
+        nama: "Fikri Maulana",
+        nim: "20252210023",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 85
+    },
+    {
+        nama: "Hani Wulandari",
+        nim: "20252210024",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 91
+    },
+    {
+        nama: "Ilham Fauzi",
+        nim: "20252210025",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 83
+    },
+    {
+        nama: "Maya Sari",
+        nim: "20252210026",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 90
+    },
+    {
+        nama: "Rafi Akbar",
+        nim: "20252210027",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1B",
+        nilai: 88
+    },
+    {
+        nama: "Vina Amelia",
+        nim: "20252210028",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1C",
+        nilai: 94
+    },
+    {
+        nama: "Yusuf Ramadhan",
+        nim: "20252210029",
+        jurusan: "Teknik Informatika",
+        kelas: "TI-1A",
+        nilai: 86
+    }
+];
+
+export default dataMahasiswa;
